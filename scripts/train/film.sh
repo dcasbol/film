cff7302bdc860732ae107e894e1079e26f2aa5d2
David <5521007+dcasbol@users.noreply.github.com>
2021-02-26 17:21:47 +0000

    add a bit of verbosity

--- scripts/train/film.sh
+++ scripts/train/film.sh
@@ -3,11 +3,11 @@
 checkpoint_path="data/film.pt"
 log_path="data/film.log"
 python scripts/train_model.py \
-  ----loader_num_workers 8 \
+  --loader_num_workers 8 \
   --checkpoint_path $checkpoint_path \
   --model_type FiLM \
   --num_iterations 20000000 \
-  --print_verbose_every 20000000 \
+  --print_verbose_every 100 \
   --checkpoint_every 11000 \
   --record_loss_every 100 \
   --num_val_samples 149991 \
